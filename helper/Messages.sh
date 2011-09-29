c417eca87a5aa5cdc0fef8679e9cf9fe261d7568
Thomas Balling Sørensen <tball.aau@gmail.com>
2011-09-29 17:45:29 +0200

    Added Message.sh to the helper directory, since the helper contains several i18n() functions

---
+++ helper/Messages.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+$EXTRACTRC `find -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp || exit 11
+$XGETTEXT `find -name \*.cpp -o -name \*.h` -o $podir/kcm_polkitactions.pot
+rm -f rc.cpp

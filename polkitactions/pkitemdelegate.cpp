e052cd0b9120176888c2c084601aa8313e403f0f
Alexander Volkov <avolkov@astralinux.ru>
2020-07-08 16:14:52 +0300

    Fix loading of dialog-password icon

--- polkitactions/pkitemdelegate.cpp
+++ polkitactions/pkitemdelegate.cpp
@@ -143,7 +143,7 @@ QSize PkItemDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelI
 /////////////////////////////
 
 PKLAItemDelegate::PKLAItemDelegate(QObject *parent) : QStyledItemDelegate(parent)
-    , m_passwordIcon("dialog-password")
+    , m_passwordIcon(QIcon::fromTheme("dialog-password"))
 {
 }
 
